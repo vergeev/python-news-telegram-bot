57a79fbe1a3c29f87fdcd5e2ec0e85d14d39139f
Pavel Vergeev <patrnk@gmail.com>
2017-03-02 20:46:25 +0300

    Make variable names in tests similar

--- sources_vk_test.py
+++ sources_vk_test.py
@@ -14,36 +14,36 @@ class TestVkSources(unittest.TestCase):
         api = self.get_vk_api()
         number_of_pages = 25
         search_queries = ['VK']
-        result = sources_vk.get_vk_public_page_list(api, search_queries, 
+        output = sources_vk.get_vk_public_page_list(api, search_queries, 
                                                     results_per_query=number_of_pages)
-        self.assertEqual(len(result), number_of_pages)
-        for public_page in result:
+        self.assertEqual(len(output), number_of_pages)
+        for public_page in output:
             self.assertIsInstance(public_page, dict)
 
     def test_get_vk_public_page_id_set(self):
         known_input = [{"name": "page1", "gid": 97616552},
                        {"name": "страница2", "gid": 77157551},
                        {"name": "page3", "gid": 104610799, "somethingelse": 1}]
-        known_output = set([97616552, 104610799, 77157551])
+        expected_output = set([97616552, 104610799, 77157551])
         output = sources_vk.get_vk_public_page_id_set(known_input)
-        self.assertEqual(output, known_output)
+        self.assertEqual(output, expected_output)
 
     def test_retrieve_vk_posts(self):
         api = self.get_vk_api()
         number_of_posts = 5
         tproger_page_id = 30666517
-        posts = sources_vk.get_last_vk_community_posts(api, tproger_page_id, 
+        result = sources_vk.get_last_vk_community_posts(api, tproger_page_id, 
                                                        count=number_of_posts)
-        self.assertEqual(len(posts), number_of_posts)
-        for post in posts:
+        self.assertEqual(len(result), number_of_posts)
+        for post in result:
             self.assertIsInstance(post, dict)
 
     def test_form_vk_post_link(self):
         page_id = -30666517
         post_id = 1472604
-        known_result = 'https://vk.com/wall-30666517_1472604'
-        result = sources_vk.form_vk_post_link(page_id, post_id)
-        self.assertEqual(result, known_result)
+        expected_output = 'https://vk.com/wall-30666517_1472604'
+        output = sources_vk.form_vk_post_link(page_id, post_id)
+        self.assertEqual(output, expected_output)
 
     def test_get_stripped_vk_posts(self):
         relevant_fields = ['date', 'text', 'link']
