8bd597d882d04a6022ede1c42e81b2fafd5833a3
Pavel Vergeev <patrnk@gmail.com>
2017-03-02 09:44:52 +0300

    Add tests for get_last_vk_community_posts

--- sources_vk_test.py
+++ sources_vk_test.py
@@ -5,12 +5,15 @@ import vk
 
 class TestVkSources(unittest.TestCase):
 
-    def test_retrieve_default_number_of_pages_from_vk(self):
-        default_number_of_pages = 20
-        search_queries = ['VK']
+    def get_vk_api(self):
         session = sources_vk.try_to_fetch_existing_session()
         session = session or sources_vk.ask_for_password_and_get_session()
-        api = vk.API(session)
+        return vk.API(session)
+        
+    def test_retrieve_default_number_of_pages_from_vk(self):
+        api = self.get_vk_api()
+        default_number_of_pages = 20
+        search_queries = ['VK']
         result = sources_vk.get_vk_public_page_list(api, search_queries)
         self.assertEqual(len(result), default_number_of_pages)
         for public_page in result:
@@ -23,6 +26,15 @@ class TestVkSources(unittest.TestCase):
         known_output = set([97616552, 104610799, 77157551])
         output = sources_vk.get_vk_public_page_id_set(known_input)
         self.assertEqual(output, known_output)
+
+    def test_retrieve_default_number_of_posts(self):
+        api = self.get_vk_api()
+        default_number_of_posts = 5
+        tproger_page_id = 30666517
+        posts = sources_vk.get_last_vk_community_posts(api, tproger_page_id)
+        self.assertEqual(len(posts), default_number_of_posts)
+        for post in posts:
+            self.assertIsInstance(post, dict)
 
 
 if __name__ == '__main__':
