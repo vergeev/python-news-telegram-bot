a79a62c3c222659e75741fe480d5a106bcee77df
Paul Vergeev <paulvergeev@gmail.com>
2017-03-03 17:06:45 +0300

    Add test for too many requests

--- vk_test.py
+++ vk_test.py
@@ -13,6 +13,13 @@ class TestVkRequest(unittest.TestCase):
         output = vk.make_vk_api_request('users.get', access_token=token)
         self.assertEqual(len(output['response']), 1)
 
+    def test_a_lot_of_api_requests_per_second(self):
+        for i in range(1, 100):
+            output = vk.make_vk_api_request('users.get', user_ids=1, fields='screen_name')
+            self.assertEqual(output['response'][0]['screen_name'], 'durov')
+
+            
+
     #def test_groups_search(self):
     #    access_token = os.environ('VK_ACCESS_TOKEN')
     #    print(groups_search(access_token, query='VK', type='page', count=10))
