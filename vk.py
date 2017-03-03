b23d7598402b50649058e051f61c202ac679f17b
Paul Vergeev <paulvergeev@gmail.com>
2017-03-03 17:40:38 +0300

    Implement group_search

--- vk.py
+++ vk.py
@@ -1,10 +1,26 @@
 import os
-import json
 import requests
+
+
+class VkRequestError(Exception): pass
+
 
 def make_vk_api_request(method, **params):
     method_url = 'https://api.vk.com/method/%s' % method
     response = requests.get(method_url, params=params)
+    response.raise_for_status()
     return response.json()
 
-#def groups_search(access_token, *, query, type, count=20):
+
+def raise_if_vk_error(json_response):
+    if 'error' in json_response:
+        error_code = json_response['error']['error_code']
+        error_msg = json_response['error']['error_msg']
+        raise VkRequestError("%d: %s" % (error_code, error_msg))
+
+
+def groups_search(access_token, *, query, type, count=20):
+    response = make_vk_api_request('groups.search', access_token=access_token,
+                                   q=query, type=type, count=count)
+    raise_if_vk_error(response)
+    return response['response'][1:]
--- vk_test.py
+++ vk_test.py
@@ -14,15 +14,22 @@ class TestVkRequest(unittest.TestCase):
         self.assertEqual(len(output['response']), 1)
 
     def test_a_lot_of_api_requests_per_second(self):
-        for i in range(1, 100):
+        for i in range(1, 50):
             output = vk.make_vk_api_request('users.get', user_ids=1, fields='screen_name')
             self.assertEqual(output['response'][0]['screen_name'], 'durov')
 
-            
+    def test_groups_search_valid(self):
+        access_token = os.environ.get('VK_ACCESS_TOKEN')
+        count = 10
+        output = vk.groups_search(access_token, query='VK', type='page', count=count)
+        self.assertEqual(len(output), count)
+        for group in output:
+            self.assertIsInstance(group, dict)
 
-    #def test_groups_search(self):
-    #    access_token = os.environ('VK_ACCESS_TOKEN')
-    #    print(groups_search(access_token, query='VK', type='page', count=10))
+    def test_groups_search_invalid(self):
+        count = 10
+        with self.assertRaises(vk.VkRequestError):
+            vk.groups_search('bad access token', query='VK', type='page', count=count)
 
 
 if __name__ == '__main__':
