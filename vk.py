2404cecd8ba8a58df9f53487888d6804842d808e
Paul Vergeev <paulvergeev@gmail.com>
2017-03-03 17:49:32 +0300

    Make groups_search more readable

--- vk.py
+++ vk.py
@@ -5,13 +5,6 @@ import requests
 class VkRequestError(Exception): pass
 
 
-def make_vk_api_request(method, **params):
-    method_url = 'https://api.vk.com/method/%s' % method
-    response = requests.get(method_url, params=params)
-    response.raise_for_status()
-    return response.json()
-
-
 def raise_if_vk_error(json_response):
     if 'error' in json_response:
         error_code = json_response['error']['error_code']
@@ -19,8 +12,17 @@ def raise_if_vk_error(json_response):
         raise VkRequestError("%d: %s" % (error_code, error_msg))
 
 
+def make_vk_api_request(method, **params):
+    method_url = 'https://api.vk.com/method/%s' % method
+    response = requests.get(method_url, params=params)
+    response.raise_for_status()
+    json_response = response.json() 
+    raise_if_vk_error(json_response)
+    return json_response
+
+
 def groups_search(access_token, *, query, type, count=20):
     response = make_vk_api_request('groups.search', access_token=access_token,
                                    q=query, type=type, count=count)
-    raise_if_vk_error(response)
-    return response['response'][1:]
+    group_list = response['response'][1:]  # the first element is the number of groups
+    return group_list
