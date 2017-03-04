0afb7cd8fd29af0a213f960feff8052e60f1252f
Paul Vergeev <paulvergeev@gmail.com>
2017-03-04 11:57:11 +0300

    Add stubs for reading/writing vk posts

--- vk_posts.py
+++ vk_posts.py
@@ -1,4 +1,7 @@
-
+import json
+import sys
+import argparse
+from os import remove
 
 def form_vk_post_link(page_id, post_id):
     return "https://vk.com/wall%d_%d" % (page_id, post_id)
@@ -14,10 +17,35 @@ def strip_irrelevant_post_info(raw_post):
 def strip_vk_posts(post_list):
     return [strip_irrelevant_post_info(post) for post in post_list]
 
+
+def load_source_ids_set(source_file):
+    return set(json.load(source_file))
+
+
+def load_existing_posts(posts_file):
+    raise NotImplemented
+
+
+def save_posts(posts, filename):
+    raise NotImplemented
+
+
+def save_with_existing_posts(posts, filename):
+    raise NotImplemented
+
+
+def get_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--infile', type=argparse.FileType('r'), default=sys.stdin,
+                        help='JSON file with vk source page ids')
+    parser.add_argument('-o', '--outfile', type=str, default='posts.json', 
+                        help='JSON file where posts will be stored')
+
+
 if __name__ == '__main__':
     args = get_argument_parser()
-    source_ids = load_data(agrs.infile)
+    source_ids = load_source_ids_set(args.infile)
     posts = get_vk_posts(source_ids)
     python_posts = filter_posts(posts, is_python_post)
     python_stripped_posts = strip_vk_posts(python_posts)
-    save_data(python_stripped_posts)
+    save_with_existing_posts(python_stripped_posts, args.outfile)
